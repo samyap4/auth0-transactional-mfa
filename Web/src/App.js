1932070ae95c2134df09add981d0e08a2c38e1e3
Sam Yapkowitz <samyapkowitz@gmail.com>
2022-05-11 16:37:04 -0500

    Update App.js

--- Web/src/App.js
+++ Web/src/App.js
@@ -198,18 +198,17 @@ export default function App() {
           </div>
           <br></br>
           {!balanceTransfered && 
-          <>
-            <button 
-              onClick={()=>loginWithRedirect({acr_values: 'http://schemas.openid.net/pape/policies/2007/06/multi-factor'})} 
-              style={{display: 'inline-block'}}
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Transfer Balance
-            </button>
-            <br></br>
-            <br></br>
-          </>
-            
+            <>
+              <button 
+                onClick={()=>loginWithRedirect({acr_values: 'http://schemas.openid.net/pape/policies/2007/06/multi-factor'})} 
+                style={{display: 'inline-block'}}
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Transfer Balance
+              </button>
+              <br></br>
+              <br></br>
+            </>
           }
           
           <LogoutButton />
