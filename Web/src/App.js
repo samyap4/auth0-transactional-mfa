fed8eac60f06aaea029799f86a5ce9ff5fd2d68e
Sam Yapkowitz <samyapkowitz@gmail.com>
2022-07-13 11:27:09 -0500

    ui tweaks

--- Web/src/App.js
+++ Web/src/App.js
@@ -210,7 +210,13 @@ export default function App() {
               <br></br>
             </>
           }
-          
+          <button 
+                onClick={()=>loginWithRedirect({acr_values: 'http://schemas.openid.net/pape/policies/2007/06/multi-factor'})} 
+                style={{display: 'inline-block'}}
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Issue MFA
+              </button>
           <LogoutButton />
         </div>
       }
